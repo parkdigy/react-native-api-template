7273384affc29d5cf73d3d7c0eb092368f91fde6
parkdigy <parkdigy@gmail.com>
2025-09-05 00:43:17 +0900

    Update

--- @database/mysql.sql
+++ @database/mysql.sql
@@ -1,10 +1,4 @@
---
--- Table structure for table `config`
---
-
 DROP TABLE IF EXISTS `config`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `config` (
   `id` int unsigned NOT NULL,
   `ios_app_version` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ios 최신 앱 버전',
@@ -19,15 +13,8 @@ CREATE TABLE `config` (
   `update_date` datetime NOT NULL COMMENT '수정 일자',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `data_key`
---
 
 DROP TABLE IF EXISTS `data_key`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `data_key` (
   `id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `data_key` bigint unsigned NOT NULL,
@@ -35,15 +22,8 @@ CREATE TABLE `data_key` (
   `update_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `faq`
---
 
 DROP TABLE IF EXISTS `faq`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `faq` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `faq_category_id` int unsigned NOT NULL,
@@ -60,15 +40,8 @@ CREATE TABLE `faq` (
   KEY `idx_fcid_s_id` (`faq_category_id`,`status`,`id`),
   KEY `idx_fcid_s_vs_id` (`faq_category_id`,`status`,`view_seq`,`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `faq_category`
---
 
 DROP TABLE IF EXISTS `faq_category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `faq_category` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -80,15 +53,8 @@ CREATE TABLE `faq_category` (
   KEY `idx_vs_id` (`view_seq`,`id`),
   KEY `idx_s_vs_id` (`status`,`view_seq`,`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `fcm_token`
---
 
 DROP TABLE IF EXISTS `fcm_token`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `fcm_token` (
   `id` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int unsigned NOT NULL,
@@ -103,15 +69,8 @@ CREATE TABLE `fcm_token` (
   KEY `idx_os` (`os`),
   KEY `idx_uid_os` (`user_id`,`os`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `notice`
---
 
 DROP TABLE IF EXISTS `notice`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `notice` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -123,51 +82,60 @@ CREATE TABLE `notice` (
   PRIMARY KEY (`id`),
   KEY `idx_s_id` (`status`,`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user`
---
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `user_key` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '회원 KEY',
-  `sns_user_id` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sns_user_id` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `nickname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '이름',
-  `email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `is_push_notification` tinyint(1) NOT NULL DEFAULT '1' COMMENT '신규 광고 및 이벤트 알림',
   `reg_os` enum('ios','aos') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `reg_type` enum('KAKAO','NAVER','GOOGLE','APPLE') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `reg_type` enum('GUEST','KAKAO','NAVER','GOOGLE','APPLE') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `reg_app_key` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `reg_install_app_key` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `login_date` datetime DEFAULT NULL,
   `resign_date` datetime DEFAULT NULL,
   `status` enum('ON','RESIGN') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '상태 (ON=사용, RESIGN=탈퇴)',
   `create_date` datetime NOT NULL COMMENT '등록 일자',
   `update_date` datetime NOT NULL COMMENT '수정 일자',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uq_user_key` (`user_key`),
+  UNIQUE KEY `user_key_UNIQUE` (`user_key`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`),
   KEY `idx_cd` (`create_date`),
   KEY `idx_s_ld` (`status`,`login_date`),
   KEY `idx_s_cd` (`status`,`create_date`),
   KEY `idx_s_rd` (`status`,`resign_date`),
   KEY `idx_s_isltnlm_ld` (`status`,`login_date`),
   KEY `idx_s_ipac_lacymd_lacpsymd` (`status`)
-) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user_login`
---
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `user_login`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_login` (
-  `login_key` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int unsigned NOT NULL,
+  `app_key` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `login_key` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `os` enum('ios','aos') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `os_version` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `build_number` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `device_id` int unsigned NOT NULL,
+  `expire_date` datetime DEFAULT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  PRIMARY KEY (`user_id`,`app_key`),
+  UNIQUE KEY `login_key_UNIQUE` (`login_key`),
+  KEY `idx_ak_uid` (`app_key`,`user_id`),
+  KEY `idx_uid_ud` (`user_id`,`update_date`),
+  KEY `idx_ud` (`update_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `user_login_sns`;
+CREATE TABLE `user_login_sns` (
+  `type` enum('KAKAO','NAVER','GOOGLE','APPLE') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sns_user_id` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `sns_access_token` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `sns_access_token_exp` datetime DEFAULT NULL,
   `sns_refresh_token` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -175,18 +143,10 @@ CREATE TABLE `user_login` (
   `expire_date` datetime DEFAULT NULL,
   `create_date` datetime NOT NULL,
   `update_date` datetime NOT NULL,
-  PRIMARY KEY (`login_key`),
-  KEY `idx_uid` (`user_id`)
+  PRIMARY KEY (`type`,`sns_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user_resign`
---
 
 DROP TABLE IF EXISTS `user_resign`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_resign` (
   `user_id` int unsigned NOT NULL,
   `reason` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
