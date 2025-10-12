b8ab19dcd5b89b43a0acab5814291b5c3e3f1b06
parkdigy <parkdigy@gmail.com>
2025-10-12 15:36:49 +0900

    Update

--- src/@types/MyRequest.ts
+++ src/@types/MyRequest.ts
@@ -6,9 +6,16 @@ export interface MyRequestUser {
   email: string;
 }
 
-export interface MyRequest extends Request {
+interface MyRequestCommon extends Request {
   $$remoteIpAddress?: string;
   $$dbTransMySql?: Knex.Transaction[];
   $$dbTransMsSql?: Knex.Transaction[];
+}
+
+export interface MyRequest extends MyRequestCommon {
   $$user?: MyRequestUser;
+}
+
+export interface MyAuthRequest extends MyRequestCommon {
+  $$user: MyRequestUser;
 }
