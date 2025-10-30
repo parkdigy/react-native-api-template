5164b6d7e6ba07badb8cef8293b7448530cb9012
parkdigy <parkdigy@gmail.com>
2025-10-30 12:51:52 +0900

    Update

--- src/common/aws/awsS3/awsS3Private.ts
+++ src/common/aws/awsS3/awsS3Private.ts
@@ -239,9 +239,10 @@ export const awsS3Private = {
    * 파일 다운로드
    * @param s3Path S3 경로
    * @param s3FileName S3 파일명
+   * @param notUseS3Path env 에서 지정한 S3_PATH 미사용 여부
    * @returns 파일 데이터
    * ******************************************************************************************************************/
-  getObject(s3Path: string, s3FileName: string) {
+  getObject(s3Path: string, s3FileName: string, notUseS3Path = false) {
     if (!s3) throw new Error('env 에 S3 (Private) 정보를 등록해야합니다.');
     if (empty(process.env.S3_PRIVATE_BUCKET)) throw new Error('env 에 S3_PRIVATE_BUCKET 값을 등록해야 합니다.');
 
@@ -249,7 +250,7 @@ export const awsS3Private = {
       s3.send(
         new GetObjectCommand({
           Bucket: process.env.S3_PRIVATE_BUCKET,
-          Key: util.url.join(s3Path, s3FileName),
+          Key: util.url.join(notUseS3Path ? '' : ifEmpty(process.env.S3_PATH, ''), s3Path, s3FileName),
         })
       )
         .then((data) => {
@@ -263,14 +264,15 @@ export const awsS3Private = {
    * 파일 삭제
    * @param s3Path S3 경로
    * @param s3FileName S3 파일명
+   * @param notUseS3Path env 에서 지정한 S3_PATH 미사용 여부
    * ******************************************************************************************************************/
-  deleteObject(s3Path: string, s3FileName: string) {
+  deleteObject(s3Path: string, s3FileName: string, notUseS3Path = false) {
     if (!s3) throw new Error('env 에 S3 (Private) 정보를 등록해야합니다.');
     if (empty(process.env.S3_PRIVATE_BUCKET)) throw new Error('env 에 S3_PRIVATE_BUCKET 값을 등록해야 합니다.');
 
     return new Promise<boolean>((resolve, reject) => {
       try {
-        const key = util.url.join(s3Path, s3FileName);
+        const key = util.url.join(notUseS3Path ? '' : ifEmpty(process.env.S3_PATH, ''), s3Path, s3FileName);
 
         const command = new DeleteObjectCommand({
           Bucket: process.env.S3_PRIVATE_BUCKET,
